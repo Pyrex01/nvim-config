11ec742e669801abcfe5d132d2367159bf6b9b37
Riyan <riyankhanpyrex01@gmail.com>
2024-12-23 04:50:46 +0000

    [feat] dbee integrated

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
+local dbee = require("dbee")
 vim.keymap.set({ "n", "i", "v", "x", "s", "o", "t", "c" }, "<C-n>", "<ESC>")
+vim.keymap.set("n", "<leader>db", dbee.open, { noremap = true, silent = true })
